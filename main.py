e933fb4f55b2c882ff99df24145c34bad8d915d6
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-02-11 12:34:36 +0000

    renaming

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pygame
 from music21 import chord, converter, midi, note
 
-from trainer.create_generator_model import GAN
+from trainer.train_generator import GAN
 from trainer.generate_music import create_midi, generate_music
 
 LATENT_DIMENSION = 1000
