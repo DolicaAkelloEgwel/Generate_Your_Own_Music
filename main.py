26ec038bd5e909cd043cf792c1edd9fdf422a891
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-02-11 12:51:30 +0000

    black and isort

--- main.py
+++ main.py
@@ -19,28 +19,23 @@ BATCH_SIZE = 24
 
 INPUT_PATH = "input"
 
+notes = get_notes(INPUT_PATH)
+n_vocab = len(set(notes))
 
-if __name__ == "__main__":
+mixer.init(MIXER_FREQ, MIXER_BIT_SIZE, MIXER_CHANNELS, MIXER_BUFFER)
 
-    notes = get_notes(INPUT_PATH)
-    n_vocab = len(set(notes))
+# Train a GAN on the midi files once
+gan = GAN()
+gan.train(notes=notes, n_vocab=n_vocab, batch_size=BATCH_SIZE)
 
-    mixer.init(MIXER_FREQ, MIXER_BIT_SIZE, MIXER_CHANNELS, MIXER_BUFFER)
+while True:
 
-    # Train a GAN on the midi files once
-    gan = GAN()
-    gan.train(notes=notes, n_vocab=n_vocab, batch_size=BATCH_SIZE)
+    # Get a midi sequence from the generator
+    generated_music = generate_music(gan.generator, LATENT_DIMENSION, notes, n_vocab)
 
-    while True:
+    # Convert midi output to bytes
+    midi_stream = midi.translate.streamToMidiFile(create_midi(generated_music))
+    midi_bytes = BytesIO(midi_stream.writestr())
 
-        # Get a midi sequence from the generator
-        generated_music = generate_music(
-            gan.generator, LATENT_DIMENSION, notes, n_vocab
-        )
-
-        # Convert midi output to bytes
-        midi_stream = midi.translate.streamToMidiFile(create_midi(generated_music))
-        midi_bytes = BytesIO(midi_stream.writestr())
-
-        # play the output
-        play_midi_bytes(midi_bytes)
+    # play the output
+    play_midi_bytes(midi_bytes)
